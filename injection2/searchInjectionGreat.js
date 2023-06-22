f1a427bd7081a42d9223a3d774a8742e9db50830
Shane Fast <shane.fast01@gmail.com>
2023-06-22 09:30:43 -0600

    Initial commit

---
+++ injection2/searchInjectionGreat.js
@@ -0,0 +1,19 @@
+const sanitize = require('mongo-sanitize');
+const validator = require('validator');
+
+userRoutes.route('/search').post(function(req, res) {
+    let search = String(req.query.search);
+    let query = {plants: search}
+
+    db.collection('plants').find(query, function (err, plants) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.render('plants', { title: 'Plants', plants: plants });
+        }
+    });
+});
+
+
+//What if we wanted to make sure it was a number? 
+//what about a date?
